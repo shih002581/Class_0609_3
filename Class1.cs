add358fc2230eef692289f07e6920c8de97b5c06
shih002581 <d11416147@gs.takming.edu.tw>
2026-06-09 16:10:52 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection.Emit;
+
+delegate int MyDel(int a, int b);
+
+public class Class1
+{
+    public static string str = "";
+	public static void Add(int a, int b)
+	{
+		str += "\n" + (a + b);
+	}
+    public static void Sub(int a, int b)
+    {
+        str += "\n" + (a - b);
+    }
+    public static void Mul(int a, int b)
+    {
+        str += "\n" + (a * b);
+    }
+    public static void Div(int a, int b)
+    {
+        str += "\n" + (a / b);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Class_0609_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDel hander;
+
+            hander = new MyDel(Class1.Sub);
+            hander += new MyDel(Class1.Add);
+            hander += new MyDel(Class1.Div);
+            hander += new MyDel(Class1.Mul);
+            hander += new MyDel(Class1.Add);
+            hander -= new MyDel(Class1.Sub);
+
+            hander(10, 20);
+            Console.WriteLine(Class1.Str);
+        }
+    }
+}
